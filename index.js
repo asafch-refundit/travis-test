6ae6039ddb3598c3ab3605adab0896ad9b8398eb
Chelouche, Asaf <asaf@refundit.tax>
2019-09-11 14:37:15 +0300

    aync

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ const _ = require('lodash');
 const knex = require('knex');
 const redis = require('async-redis');
 
-const redisClient = redis.createClient();
-const db = knex({
-                client: 'pg',
-                searchPath: ['main'],
-                pool: {
-                    min: 1,
-                    max: 1
-                },
-                connection: {
-                    host: 'localhost',
-                    port: 5432,
-                    user: 'potgres',
-                    database: 'refundit'
-                },
-                debug: true
-            });
+(async () => {
+	const redisClient = redis.createClient();
+	const db = knex({
+	                client: 'pg',
+	                searchPath: ['main'],
+	                pool: {
+	                    min: 1,
+	                    max: 1
+	                },
+	                connection: {
+	                    host: 'localhost',
+	                    port: 5432,
+	                    user: 'potgres',
+	                    database: 'refundit'
+	                },
+	                debug: true
+	            });
 
-console.log(`Is {} empty? ${_.isEmpty({})}`);
-console.log(`Hello, world!`);
+	console.log(`Is {} empty? ${_.isEmpty({})}`);
+	console.log(`Hello, world!`);
 
-const setR = await redisClient.set('key', 17);
-const get = await redisClient.get('key');
+	const setR = await redisClient.set('key', 17);
+	const get = await redisClient.get('key');
 
-console.log(`get: ${get}`);
-
+	console.log(`get: ${get}`);
+})();
